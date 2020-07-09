2b1caaf269e321e35d0fbdbf302434699d3e0171
benwatsonuk <hello@benwatson.eu>
2020-07-09 08:43:19 +0100

    Added Alpha banner to layout.html

--- app/routes.js
+++ app/routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 require('./routes/0.js')(router)
+require('./routes/1.js')(router)
 
 module.exports = router
