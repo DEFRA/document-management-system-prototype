719aafc712f1da77843054acea76917db5d631ec
benwatsonuk <hello@benwatson.eu>
2020-07-21 11:34:36 +0100

    Added V2 folder

---
+++ app/routes/2.js
@@ -0,0 +1,72 @@
+const version = '1'
+const versionDirectory = 'v' + version
+const previousVersionDirectory = 'v0'
+const pf = require('@benwatsonuk/page-flow')
+let pageFlow = require('../data/pages')
+let userFlow = require('../views/' + versionDirectory + '/user-flows.json')
+let previousUserFlow = require('../views/' + previousVersionDirectory + '/user-flows.json')
+
+const documentData = require('../views/' + versionDirectory + '/data/documents.json');
+
+module.exports = function (router) {
+
+    router.get(['/' + versionDirectory + '/page-flow/'], function (req, res) {
+        res.render('./includes/page-flow.html',
+            {
+                pageIndex: pf.renderPageIndex(pageFlow)
+            }
+        )
+    })
+
+    router.get(['/' + versionDirectory + '/user-flow/'], function (req, res) {
+        res.render('./includes/user-flow.html',
+            {
+                userFlow: pf.renderUserFlow(userFlow, pageFlow, previousUserFlow, version)
+            }
+        )
+    })
+
+    router.get(['/' + versionDirectory + '/user-flow/:journeyId/:theJourneyDirectory/:page', '/' + versionDirectory + '/user-flow/:journeyId/:theJourneyDirectory/:stage/:page'], function (req, res) {
+        res.render('./includes/user-flow.html',
+            {
+                userFlow: pf.renderUserFlowPage(pageFlow, userFlow, req.params.page, req.params.theJourneyDirectory, versionDirectory, req.params.journeyId, req.params.stage)
+            }
+        )
+    })
+    // include common / userflows.js
+
+    router.get(['/' + versionDirectory + '/search/search-results', '/' + versionDirectory + '/search/search-results/:variant'], (req, res) => {
+        let thePageObject = documentData
+        let pageVariant = req.params.variant || 1
+        let searchType = req.query.searchType || 1
+        let filterType
+        let resultsType
+        if (pageVariant == 3) {
+            filterType = req.query.filterType || 3
+            resultsType = req.query.resultsType || 1
+            searchType = req.query.resultsType || 2
+        } else if (pageVariant == 2) {
+            filterType = req.query.filterType || 2
+            resultsType = req.query.resultsType || 2
+        } else {
+            filterType = req.query.filterType || 1
+            resultsType = req.query.resultsType || 1
+        }
+        res.render(versionDirectory + '/search/search-results.html', {
+            pageObject: thePageObject,
+            searchType: searchType,
+            pageVariant: pageVariant,
+            resultsType: resultsType,
+            filterType: filterType
+        })
+    })
+    //
+    // router.get(['/' + versionDirectory + '/search-results/:variant'], function (req, res) {
+    //     let thePageObject = documentData
+    //     thePageObject.filterType = req.params.variant
+    //     res.render( versionDirectory + '/search-results.html', {
+    //         pageObject: thePageObject
+    //     })
+    // })
+
+}
