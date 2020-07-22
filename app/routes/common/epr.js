bae76441ab8355f8139903b905d4f0d2b5ff1648
benwatsonuk <hello@benwatson.eu>
2020-07-22 13:26:01 +0100

    Updated references to suit

--- app/routes/common/epr.js
+++ app/routes/common/epr.js
@@ -1,6 +1,6 @@
 module.exports = function (router, versionDirectory, eprData) {
 
-    router.get(['/' + versionDirectory + '/entry-points/ea/existing/results-details'], function (req, res) {
+    router.get(['/' + versionDirectory + '/entry-points/ea/results-details'], function (req, res) {
         let thePageObject = {}
         let permitId = req.query.permitNumber || "EAWML403958"
         thePageObject = eprData.find((item) => {return item.caseReference === permitId})
@@ -10,7 +10,7 @@ module.exports = function (router, versionDirectory, eprData) {
         )
     })
 
-    router.get(['/' + versionDirectory +'/entry-points/ea/existing/results'], function (req, res) {
+    router.get(['/' + versionDirectory +'/entry-points/ea/results'], function (req, res) {
         let thePageObject = {}
         thePageObject.results = eprData
         res.render(versionDirectory + '/entry-points/ea/existing/results.html', {
